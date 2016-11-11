6358f6e6388c7bacc5c91e1586a1d7b0f0105e0a
shohinaea <eliz-z-za@yandex.ru>
2016-11-11 19:18:44 +0300

    Initial commit

---
+++ src/com/company/Card.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by student2 on 11.11.16.
+ */
+public class Card {
+    Suit suit;
+    Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return ""+value+" of "+suit;
+    }
+}
